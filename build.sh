0bc172ddb959eb54bba0a775e6cd1877642b59a4
Leoysen <leoyzen@gmail.com>
2013-11-06 21:32:06 +0800

    Add Strip Script

--- build.sh
+++ build.sh
@@ -9,12 +9,16 @@ find -name '*.ko' -exec cp -av {} ../bootimg_tools_Charm/ramdisk_advanced/lib/mo
 mv arch/arm/boot/zImage ../bootimg_tools_Charm/
 cd ../bootimg_tools_Charm/ramdisk_advanced
 rm -f ../*.cpio.gz
+echo "Strip Modules"
+arm-eabi-strip -S lib/modules/*.ko
+echo "Packing Ramdisk Of OC Version"
 find . | cpio -o -H newc | gzip > ../initramfs-Advanced-OC.cpio.gz
+echo "Packing Ramdisk Of Standard Version"
 mv init.scripts.sh ../
 find . | cpio -o -H newc | gzip > ../initramfs-Advanced.cpio.gz
 cd ../
 mv init.scripts.sh ramdisk_advanced
-echo "Pack Sense boot.img"
+echo "Pack Sense Boot Image"
 ./mkbootimg --kernel zImage --ramdisk initramfs-Advanced-OC.cpio.gz --base 0x13f00000 --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=primou' --pagesize 4096 -o Charm-Kiss-date-Advanced-OC-3.0.101.img
 sleep 5
 ./mkbootimg --kernel zImage --ramdisk initramfs-Advanced.cpio.gz --base 0x13f00000 --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=primou' --pagesize 4096 -o Charm-Kiss-date-Advanced-3.0.101.img
