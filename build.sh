4cf25580179ea995c9acf64314880f5902ba1230
Leoyzen <leoyzen@gmail.com>
2014-01-07 11:22:25 +0800

    Some Build Fix

--- build.sh
+++ build.sh
@@ -4,15 +4,15 @@ echo "Start Compiling"
 echo "Compile KERNEL for CM9/CM10"
 make clean mrproper
 make charm-kiss-cm10_defconfig
-colormake -j84
+colormake -j5
 echo "Copy Modules to CM10 Ramdisk"
 find -name '*.ko' -exec cp -av {} ../bootimg_tools_Charm/ramdisk_cm10/lib/modules/ \;
 mv arch/arm/boot/zImage ../bootimg_tools_Charm/
 cd ../bootimg_tools_Charm
-echo "Strip Modules"
-arm-eabi-strip -S lib/modules/*.ko
 echo "Pack CM10 Ramdisk"
 cd ramdisk_cm10
+echo "Strip Modules"
+arm-eabi-strip -S lib/modules/*.ko
 rm -f ../*.cpio.gz
 find . | cpio -o -H newc | gzip > ../initramfs-cm10.cpio.gz
 cd ../
